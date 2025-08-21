38bb8d77fee95f6565ce5758f1ddf3e5c1e25e1c
Lawrence Taylor <lawrencetaylor2nd@gmail.com>
2025-08-21 15:10:40 -0400

    Created initial tables to support authentication

--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,1 +1,19 @@
 // Export the database schema for drizzle-kit
+export {
+  roleEnum,
+  users,
+  organization,
+  roles,
+  invites,
+  accounts,
+  sessions,
+  verificationTokens,
+  authenticators,
+  usersRelations,
+  roleRelations,
+  inviteRelations,
+  accountRelations,
+  sessionRelations,
+  organizationRelations,
+ 
+} from "./authtables";
